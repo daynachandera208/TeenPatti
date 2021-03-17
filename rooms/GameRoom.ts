4536144c2313d36313717b09dab7b8465b1629aa
daynachandera208 <73104489+daynachandera208@users.noreply.github.com>
2021-03-17 11:20:52 +0530

    Update GameRoom.ts

--- rooms/GameRoom.ts
+++ rooms/GameRoom.ts
@@ -270,8 +270,9 @@ this.onMessage("SetMyCurrentWinProbability",(client,winprobability)=>{
   }
 
   async onAuth (client, options) {
-    console.log("onAuth(), options!", options);
-    return await User.findById(verifyToken(options.token)._id);
+  //  console.log("onAuth(), options!", options);
+   // return await User.findById(verifyToken(options.token)._id);
+ return true;
   }
 
 
